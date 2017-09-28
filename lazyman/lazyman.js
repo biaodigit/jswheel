40c6cf250b0da979cbde0ce62855a2827553514d
luowendi <luowendi@umier.com.cn>
2017-09-28 16:53:30 +0800

    优化 lazyman

--- lazyman/lazyman.js
+++ lazyman/lazyman.js
@@ -1,58 +1,70 @@
-/**
- * Created by di on 2017/6/27.
- */
-let Human = function (_name) {
-    return new human(_name)
-}
-let human = function (people) {
-    this.tasks = [];
-    this.people = people;
-    let self = this;
-    let fn = function () {
-        console.log('你好' + people + '!');
-        self.next()
+let Human = (name) => {
+    if(!name || typeof name !== 'string'){
+        return
     }
-    this.tasks.push(fn);
-    setTimeout(function () {
-        self.next();
-    }, 0)
+
+    return new human(name)
 }
-human.prototype.next = function () {
-    if (this.tasks.length === 0) {
-        return;
+
+
+class human {
+    constructor(name) {
+        this.people = name;
+        this.tasks = [];
+
+        let fn = () => {
+            console.log(`你好${name}`);
+            this.next()
+        }
+        this.tasks.push(fn);
+
+        setTimeout(() => {
+            this.next()
+        },0)
     }
-    let fn = this.tasks.shift();
-    fn();
-}
-human.prototype.sleep = function (time) {
-    let self = this;
-    let fn = function () {
-        setTimeout(function () {
-            console.log(self.people + '休息' + time + '秒');
-            self.next();
-        }, time * 1000)
+
+    next() {
+        if(this.tasks.length === 0){
+            return;
+        }
+
+        var fn = this.tasks.shift();
+        fn()
     }
-    this.tasks.push(fn);
-    return this;
-}
-human.prototype.eat = function (food) {
-    let self = this;
-    let fn = function () {
-        console.log(self.people + '吃了' + food);
-        self.next();
+
+    sleep(time) {
+        let fn = () => {
+            setTimeout(() => {
+                console.log(`${this.people}休息了${time}秒`);
+                this.next()
+            },time*1000)
+        }
+        this.tasks.push(fn)
+        return this
     }
-    this.tasks.push(fn);
-    return this;
-}
-human.prototype.sleepFirst = function (time) {
-    let self = this;
-    let fn = function () {
-        setTimeout(function () {
-            console.log(self.people + '开始等待' + time + '秒');
-            self.next();
-        },time*1000);
+
+    eat(food) {
+        let fn = ()=> {
+            console.log(`${this.people}吃了${food}`);
+            this.next()
+        }
+
+        this.tasks.push(fn)
+        return this
     }
-    this.tasks.unshift(fn);
-    return this;
+
+    sleepFirst(time) {
+        let fn = () => {
+            setTimeout(() => {
+                console.log(`${this.people}开始等待${time}秒`)
+                this.next()
+            },time*1000)
+        }
+
+        this.tasks.unshift(fn)
+        return this
+    }
 }
-Human('小明').sleep(2).eat('早餐').sleep(1).sleepFirst(1);
+
+Human('蜜瓜').sleep(2).eat('早餐').sleep(1).sleepFirst(1);
+
